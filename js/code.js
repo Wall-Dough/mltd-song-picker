5b43a6d6630db6d1fcb2e09fe62f084fb3dd7d11
Wall-Dough <awalden0808@gmail.com>
2018-09-11 04:42:13 -0500

    Add some translations

--- js/code.js
+++ js/code.js
@@ -137,6 +137,7 @@ function buildSongList() {
                         .setCover("https://www.project-imas.com/w/images/3/3f/TheIdolmastersong-logo.jpg"));
   songs.push(new Song().setTitle("&#x81ea;&#x5206;REST@RT")
                         .setRomanized("Jibun REST@RT")
+                        .setTranslated("Myself REST@RT")
                         .setType(SongTypes.ALL)
                         .setCover("https://www.project-imas.com/w/images/9/9d/Jibun.jpg"));
   songs.push(new Song().setTitle("READY!!")
@@ -144,6 +145,7 @@ function buildSongList() {
                         .setCover("https://www.project-imas.com/w/images/1/10/READY%21%21.png"));
   songs.push(new Song().setTitle("&#x5f85;&#x3061;&#x53d7;&#x3051;&#x30d7;&#x30ea;&#x30f3;&#x30b9;")
                         .setRomanized("Machiuke Prince")
+                        .setTranslated("Awaited Prince")
                         .setType(SongTypes.ALL)
                         .setCover("https://www.project-imas.com/w/images/8/83/MachiukePrince.jpg"));
   songs.push(new Song().setTitle("ToP!!!!!!!!!!!!!")
@@ -155,6 +157,7 @@ function buildSongList() {
 
   songs.push(new Song().setTitle("Marionette&#x306f;&#x7720;&#x3089;&#x306a;&#x3044;")
                         .setRomanized("Marionette wa Nemuranai")
+                        .setTranslated("Marionette Never Sleeps")
                         .setType(SongTypes.ANGEL)
                         .setCover("https://www.project-imas.com/w/images/5/58/Marionette_wa_Nemuranai_Logo.png"));
   songs.push(new Song().setTitle("Good-Sleep, Baby&#x2661;")
@@ -168,34 +171,42 @@ function buildSongList() {
                         .setCover("https://www.project-imas.com/w/images/0/04/Angelic_Parade%E2%99%AA_Logo.jpg"));
   songs.push(new Song().setTitle("&#x30c9;&#x30ea;&#x30fc;&#x30e0;&#x30c8;&#x30e9;&#x30d9;&#x30e9;&#x30fc;")
                         .setRomanized("Doriimu Toraberaa")
+                        .setTranslated("Dream Traveler")
                         .setType(SongTypes.ANGEL)
                         .setCover("https://www.project-imas.com/w/images/d/d1/Dream_Traveler_Logo.png"));
   songs.push(new Song().setTitle("&#x8679;&#x8272;letters")
                         .setRomanized("Nijiiro letters")
+                        .setTranslated("Rainbow-colored letters")
                         .setType(SongTypes.ANGEL)
                         .setCover("https://www.project-imas.com/w/images/f/f8/Nijiiro_letters_logo.png"));
   songs.push(new Song().setTitle("&#x661f;&#x5c51;&#x306e;&#x30b7;&#x30f3;&#x30d5;&#x30a9;&#x30cb;&#x30a2;")
                         .setRomanized("Hoshikuzu no Shinfonia")
+                        .setTranslated("Stardust Symphonia")
                         .setType(SongTypes.ANGEL)
                         .setCover("https://www.project-imas.com/w/images/9/91/Hoshikuzu_no_Symphonia_Logo.png"));
   songs.push(new Song().setTitle("&#x82b1;&#x3056;&#x304b;&#x308a;Weekend&#x273f;")
                         .setRomanized("Hanazakari Weekend&#x273f;")
+                        .setTranslated("Blooming Weekend&#x273f;")
                         .setType(SongTypes.ANGEL)
                         .setCover("https://www.project-imas.com/w/images/0/04/Jacket_hzwend.png"));
   songs.push(new Song().setTitle("&#x30b5;&#x30f3;&#x30ea;&#x30ba;&#x30e0;&#x30fb;&#x30aa;&#x30fc;&#x30b1;&#x30b9;&#x30c8;&#x30e9;&#x266a;")
                         .setRomanized("San Rizumu Ookesutora&#x266a;")
+                        .setTranslated("Sun Rhythm Orchestra")
                         .setType(SongTypes.ANGEL)
                         .setCover("https://www.project-imas.com/w/images/8/8a/Sun_Rhythm_Orchestra_Logo.png"));
   songs.push(new Song().setTitle("&#x604b;&#x306e;Lesson&#x521d;&#x7d1a;&#x7de8;")
                         .setRomanized("Koi no Lesson Shokyuuhen")
+                        .setTranslated("Love Lesson: Elementary Level Edition")
                         .setType(SongTypes.ANGEL)
                         .setCover("https://www.project-imas.com/w/images/9/95/Koi_no_Lesson_Shokyuuhen_Logo.jpg"));
   songs.push(new Song().setTitle("&#x30cf;&#x30df;&#x30f3;&#x30b0;&#x30d0;&#x30fc;&#x30c9;")
                         .setRomanized("Hamingubaado")
+                        .setTranslated("Hummingbird")
                         .setType(SongTypes.ANGEL)
                         .setCover("https://www.project-imas.com/w/images/d/d1/Hummingbird_Logo.png"));
   songs.push(new Song().setTitle("&#x30c8;&#x30ad;&#x30e1;&#x30ad;&#x306e;&#x97f3;&#x7b26;&#x306b;&#x306a;&#x3063;&#x3066;")
                         .setRomanized("Tokimeki no Onpu ni Natte")
+                        .setTranslated("Become the Notes of Excitement")
                         .setType(SongTypes.ANGEL)
                         .setCover("https://www.project-imas.com/w/images/7/7a/Tokimeki_no_Onpu_ni_Natte.png"));
   songs.push(new Song().setTitle("Happy Darling")
@@ -206,10 +217,12 @@ function buildSongList() {
                         .setCover("https://www.project-imas.com/w/images/9/92/Dear_logo.png"));
   songs.push(new Song().setTitle("&#x30cf;&#x30c3;&#x30d4;&#xff5e;&#x20;&#x30a8;&#x30d5;&#x30a7;&#x30af;&#x30c8;&#xff01;")
                         .setRomanized("Happi~ Efekuto!")
+                        .setRomanized("Happy~ Effect!")
                         .setType(SongTypes.ANGEL)
                         .setCover("https://www.project-imas.com/w/images/5/52/Jacket_hpyeft.png"));
   songs.push(new Song().setTitle("&#x60f3;&#x3044;&#x306f;Carnaval")
                         .setRomanized("Omoi wa Carnaval")
+                        .setTranslated("Feelings Are a Carnaval")
                         .setType(SongTypes.ANGEL));
 
   songs.push(new Song().setTitle("Blue Symphony")
@@ -223,10 +236,12 @@ function buildSongList() {
                         .setCover("https://www.project-imas.com/w/images/5/5c/Shooting_Stars_Logo.jpg"));
   songs.push(new Song().setTitle("FairyTale&#x3058;&#x3083;&#x3044;&#x3089;&#x308c;&#x306a;&#x3044;")
                         .setRomanized("FairyTale ja Irarenai")
+                        .setTranslated("It Can't be a FairyTale")
                         .setType(SongTypes.FAIRY)
                         .setCover("https://www.project-imas.com/w/images/1/18/FairyTale_ja_Irarenai_Logo.jpg"));
   songs.push(new Song().setTitle("&#x660f;&#x304d;&#x661f;&#x3001;&#x9060;&#x3044;&#x6708;")
                         .setRomanized("Kuraki Hoshi, Tooi Tsuki")
+                        .setTranslated("Gloomy Star, Distant Moon")
                         .setType(SongTypes.FAIRY)
                         .setCover("https://www.project-imas.com/w/images/6/6f/Kuraki_Hoshi%2C_Tooi_Tsuki_Logo.png"));
   songs.push(new Song().setTitle("Melty Fantasia")
@@ -260,6 +275,9 @@ function buildSongList() {
   songs.push(new Song().setTitle("WHY?")
                         .setType(SongTypes.FAIRY)
                         .setCover("https://www.project-imas.com/w/images/7/71/Jacket_why000.png"));
+  songs.push(new Song().setTitle("brave HARMONY")
+                        .setType(SongTypes.FAIRY)
+                        .setCover("https://www.project-imas.com/w/images/8/84/Jacket_braveh.png"));
 
   songs.push(new Song().setTitle("&#x30b3;&#x30b3;&#x30ed;&#x304c;&#x304b;&#x3048;&#x308b;&#x5834;&#x6240;")
                         .setRomanized("Kokoro ga Kaeru Basho")
